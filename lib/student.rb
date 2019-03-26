f6dbc4b78eb92d074078a4ab1ce384273651571d
David Charles <ratlover03@gmail.com>
2019-03-26 20:31:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,11 @@
 require_relative "../config/environment.rb"
 class Student
+  attr_accessor :name, :grade
 
+   def initialize (name, grade, id = nil)
+
+    @id = id
+    @name = name
+    @grade = grade
+  end
 end
